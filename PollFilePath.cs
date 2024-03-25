4b6f856fc6e81a7505ed870bf74f72c60dd77c99
Jacob Roberts <jacobo0421@gmail.com>
2024-03-25 02:32:01 -0400

    Initial commit

---
+++ PollFilePath.cs
@@ -0,0 +1,83 @@
+﻿using System.Timers;
+using System.Xml;
+using System.Xml.Linq;
+using Timer = System.Timers.Timer;
+
+namespace FileWatcher
+{
+    public class PollFilePath
+    {
+        private readonly Timer _timer;
+        private XElement _config;
+
+        private readonly string configPath = @"C:\Users\Jacob\Documents\C# Projects\FileSystemWatcher\FileSystemWatcher\FileSystemWatcher.config";
+
+        private List<WatcherJob> _watchers = new List<WatcherJob>();
+
+        public PollFilePath()
+        {
+            int TimerRefreshSeconds = 1;
+
+            //Initiate Timer
+            _timer = new Timer(1000 * TimerRefreshSeconds) { AutoReset = true };
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+
+            //check XML for any batch windows that are open and not active
+            var batchJobs = _config.Descendants().Where(x => x.Name == "BatchJob");
+
+            foreach (var job in batchJobs)
+            {
+                TimeOnly startTime = TimeOnly.Parse(job.Element("WindowStart").Value);
+                TimeOnly endTime = TimeOnly.Parse(job.Element("WindowEnd").Value);
+                TimeOnly currentTime = TimeOnly.Parse(DateTime.Now.ToLongTimeString());
+
+                if(!(currentTime > startTime && currentTime < endTime))
+                {
+                    job.Element("IsActive").Value = "0";
+                    Console.WriteLine($"Job ID {job.Element("JobID").Value} is not within active window.");
+                    continue;
+                }
+
+                //Check if job is currently running
+                if (job.Element("IsActive").Value == "1")
+                {
+                    Console.WriteLine($"Job ID {job.Element("JobID").Value} is already running.");
+                    continue;
+                }
+
+                //
+
+                //instantiate new watcherJob
+                WatcherJob newJob = new WatcherJob(job.Element("JobType").Value, job.Element("InputPath").Value, job.Element("DestinationPath").Value, job.Element("FileNamePattern").Value);
+                _watchers.Add(newJob);
+
+                //Temporarily updating value while the program is running. Would normally like this to be updated in a DB or other source instead of directly 
+                job.Element("IsActive").Value = "1";
+            }
+
+            //Check if any are active and window has closed
+        }
+
+        public void Start()
+        {
+            //Load Config XML
+            string configStr = File.ReadAllText(configPath);
+            _config = XElement.Parse(configStr);
+
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            foreach (WatcherJob job in _watchers)
+            {
+                job.Dispose();
+            }
+            _timer.Stop();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Topshelf;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<PollFilePath>(s =>
+                {
+                    s.ConstructUsing(pollFilePath => new PollFilePath());
+                    s.WhenStarted(pollFilePath => pollFilePath.Start());
+                    s.WhenStopped(pollFilePath => pollFilePath.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("FileSystemWatcher");
+                x.SetDisplayName("FileSystemWatcher");
+                x.SetDescription("Sample File polling service to perform actions on files in certain directories.");
+            });
+
+            int exitCodeValue = Convert.ToInt32(exitCode);
+
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
